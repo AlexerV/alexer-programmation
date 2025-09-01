ff25dc09e8b69e858ccfdf723b04582a6cd5ce78
Alexer <41583014+AlexerV@users.noreply.github.com>
2025-09-01 15:02:24 +0200

    Create graph.py

---
+++ code/graph.py
@@ -0,0 +1,13 @@
+from matplotlib import pyplot as plt
+
+# Les valeurs pour l'axe X
+x = [1, 2, 3, 4]
+
+# Les valeurs pour l'axe Y
+y = [2, 4, 8, 16]
+
+# Fonction pour créer le graph
+plt.plot(x, y)
+
+# Function pour montrer le graph
+plt.show()
