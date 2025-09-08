46194e8080446436f287cef5c314e27faf957935
Alexer <41583014+AlexerV@users.noreply.github.com>
2025-09-08 15:08:34 +0200

    Delete code directory

--- code/graph.py
+++
@@ -1,9 +0,0 @@
-from matplotlib import pyplot as plt  # Importation de la bibliothèque matplotlib pour tracer des graphiques
-
-x = [1, 2, 3, 4]  # Liste représentant les coordonnées de l'axe X
-
-y = [2, 4, 8, 16]  # Liste représentant les coordonnées de l'axe Y, qui sont les valeurs correspondantes de l'axe X
-
-plt.plot(x, y)  # Trace le graphique avec les valeurs de x sur l'axe des X et les valeurs de y sur l'axe des Y
-
-plt.show()  # Affiche le graphique dans une fenêtre de l'interface graphique de matplotlib
